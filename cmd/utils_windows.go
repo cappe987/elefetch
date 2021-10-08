7f3305308d7864be87fa70959ac010e5c8867a93
burntcarrot <aadhav.n1@gmail.com>
2021-10-08 10:45:06 +0530

    feat: add windows support

---
+++ cmd/utils_windows.go
@@ -0,0 +1,27 @@
+// +build windows
+
+package cmd
+
+func OS() string {
+	// use wmic for getting caption with OS name
+	out, _ := exec.Command("wmic", "os", "get", "caption").Output()
+	windowsVer := strings.TrimSpace(strings.Split(string(out), "\r\r\n")[1])
+
+	return strings.TrimPrefix(windowsVer, "Microsoft ")
+}
+
+func Kernel() string {
+	// use wmic for getting OS version
+	out, _ := exec.Command("wmic", "os", "get", "Version").Output()
+	kernelVer := strings.TrimSpace(strings.Split(string(out), "\r\r\n")[1])
+
+	return kernelVer
+}
+
+func Shell() string {
+	return "cmd.exe"
+}
+
+func WM() string {
+	return "Metro"
+}
