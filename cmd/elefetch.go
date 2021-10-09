4b99546ef01cc43102c9e93b30f9363e7e95c41e
Casper Andersson <36729780+cappe987@users.noreply.github.com>
2021-10-09 20:12:27 +0200

    Added suport for custom ascii with flag --source=path/to/file.
    Also added TrimSpace to all data fields, as my i3wm was bringing an
    extra newline at the end.
    
    Added padding length variable to printColors()

--- cmd/elefetch.go
+++ cmd/elefetch.go
@@ -2,9 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
+
+var AsciiPath string
 
 // elefetch command
 var EleFetch = &cobra.Command{
@@ -26,8 +30,41 @@ var ele = `
      |__|   |__| -
 `
 
+func customAscii(path string){
+	values := [5]string{UserText(), OSText(), KernelText(), ShellText(), WMText()}
+	path = strings.Replace(path, "~", os.Getenv("HOME"), 1) // Handles paths using ~/ syntax
+	art, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Printf("Error: no such file '%s'", path)
+		return
+	}
+
+	lines := strings.Split(string(art), "\n")
+	lines = lines[:len(lines)-1] // Cut off the extra empty line that is always there
+
+	padLength := len(lines[0])
+	fill := strings.Repeat(" ", padLength)
+	// Add extra lines if the art is fewer lines than data
+	for i := len(lines); i < 5; i++ {
+		lines = append(lines, fill)
+	}
+
+	// Format data strings
+	for i := 0; i < 5; i++ {
+		lines[i] = fmt.Sprintf("%s %s", lines[i], values[i])
+	}
+
+	lines = append(lines, "") 
+	fmt.Print(strings.Join(lines, "\n"))
+	printColors(padLength + 2)
+}
+
 // elefetch fetches system info
 func elefetch() {
+	if AsciiPath != "" {
+		customAscii(AsciiPath)
+		return
+	}
 	fmt.Printf(
 		ele,
 		UserText(),
@@ -36,5 +73,5 @@ func elefetch() {
 		ShellText(),
 		WMText(),
 	)
-	printColors()
+	printColors(25)
 }
